71f7c8aee27c024ac2d04e8787292e70ea96a5b3
Austen Stone <miltonlaxer@gmail.com>
2021-09-12 11:46:43 -0400

    cleanup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cube Field
+
+Remake of the original [Cubefield](https://www.cubefield.org.uk/).
+
+## [View Demo](http://cubefield.austen.info/)
+
+#### Run
+`npm run serve`
+
+#### Build
+`npm run build`
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,15 +6,6 @@ module.exports = {
     entry: {
         index: './src/index.js'
     },
-    watch: true,
-    watchOptions: {
-        ignored: '**/node_modules',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        hot: true,
-    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
