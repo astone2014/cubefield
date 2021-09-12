fea4c0d5d4eb01c85fe6737640eeb7d652181e65
Austen Stone <miltonlaxer@gmail.com>
2021-09-12 12:14:46 -0400

    cleanup and add stats

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import "./style.css";
+import * as Stats from 'stats.js';
+
+var stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -15,8 +20,8 @@ document.body.appendChild(renderer.domElement);
 // const helper = new THREE.CameraHelper(camera);
 // scene.add(helper);
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
 
 // var gridXZ = new THREE.GridHelper(100, 10);
 // scene.add(gridXZ);
@@ -32,55 +37,41 @@ scene.add(light.target);
 const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(ambientLight);
 
-const geometry = new THREE.PlaneGeometry(200, 200);
-const material = new THREE.MeshBasicMaterial({ color: 0x2d373d, side: THREE.DoubleSide });
-const plane = new THREE.Mesh(geometry, material);
-plane.rotateZ(90)
-// plane.rotateX(90)
-// scene.add(plane);
-
 const playerGeometry = new THREE.OctahedronGeometry();
 const playerMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff })
 const player = new THREE.Mesh(playerGeometry, playerMaterial);
 scene.add(player);
 
-// 
-let speed = 0;
-let maxCubes = 100;
-let cubes = [];
-
 const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.set(0, 0, 0);
 controls.target = player.position;
-// controls.maxDistance = 5;
+controls.maxDistance = 10;
 controls.update();
 
 camera.position.y = 4;
-camera.position.x = 10;
+camera.position.x = -10;
 // camera.position.z = 5;
 
+let cubes = [];
+let maxCubes = 500;
+let cubeFieldSize = 300;
+let cubeFieldOffsetX = 50;
 function animate() {
+    stats.begin();
     while (cubes.length < maxCubes) {
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshPhongMaterial({ color: 0xc10037 });
         const cube = new THREE.Mesh(geometry, material);
 
-        let size = 100;
-        let offset = 50;
-        cube.position.x += (Math.random() * size) - size - offset;
+        cube.position.x -= (Math.random() * cubeFieldSize) - cubeFieldSize - cubeFieldOffsetX;
         // cube.position.y += (Math.random() * 10) - 5;
-        cube.position.z += (Math.random() * size) - (size / 2);
+        cube.position.z -= (Math.random() * cubeFieldSize * 3) - (cubeFieldSize * 3 / 2);
         scene.add(cube);
         cubes.push(cube);
     }
     cubes.forEach((cube, i) => {
-        // camera.position.z -= 0.01;
-        // cube.rotation.x += 0.01;
-        // cube.rotation.y += 0.01;
-        // cube.position.z += 0.05;
-        cube.position.x += 0.05;
-        // cube.position.y += 0.001;
-        if (cube.position.x > 20) {
+        cube.position.x -= 0.3;
+        if (cube.position.x < -cubeFieldOffsetX) {
             scene.remove(cube);
             cubes.splice(i, 1);
         }
@@ -92,6 +83,7 @@ function animate() {
     // camera.getWorldDirection(cameraDirection);
     // camera.position.addScaledVector(cameraDirection, speed);
 
+    stats.end();
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
@@ -105,12 +97,12 @@ document.addEventListener("mousemove", (e) => {
 });
 
 document.addEventListener("keydown", (e) => {
-    console.log('key', e)
     switch (e.key) {
         case 'w':
         case 'ArrowUp':
             player.position.x += 1;
             break;
+        case 'ArrowLeft':
         case 'a':
             player.position.z += 1;
             break;
@@ -118,9 +110,9 @@ document.addEventListener("keydown", (e) => {
         case 'ArrowDown':
             player.position.x -= 1;
             break;
+        case 'ArrowRight':
         case 'd':
             player.position.z -= 1;
-            break;
+            break; d
     }
-    console.log(speed);
 }, false);
